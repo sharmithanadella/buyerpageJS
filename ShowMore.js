eba04a21779de5cb489c8965fec6f60407a466b7
sharmithanadella <77664897+sharmithanadella@users.noreply.github.com>
2024-03-19 23:01:32 +0530

    ShowMore.js

---
+++ ShowMore.js
@@ -0,0 +1,28 @@
+import { useRouter } from "next/router";
+import { updateSearchParams } from "@utils";
+import CustomButton from "@components/CustomButton";
+
+const ShowMore = ({ pageNumber, isNext }) => {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+    const newPathname = updateSearchParams("limit", `${newLimit}`);
+    router.push(newPathname);
+  };
+
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          btnType="button"
+          title="Show More"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ShowMore;
