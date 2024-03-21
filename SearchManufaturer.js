e3dbdb37c63bb60b8112cfd54c20a47af8ab7504
sharmithanadella <77664897+sharmithanadella@users.noreply.github.com>
2024-03-21 09:16:29 +0530

    Update SearchManufaturer.js

--- SearchManufaturer.js
+++ SearchManufaturer.js
@@ -1,6 +1,49 @@
 import { Fragment, useState } from "react";
 import { Combobox, Transition } from "@headlessui/react";
-import { manufacturers } from "@constants";
+
+const manufacturers = [
+  "Acura",
+  "Alfa Romeo",
+  "Aston Martin",
+  "Audi",
+  "Bentley",
+  "BMW",
+  "Buick",
+  "Cadillac",
+  "Chevrolet",
+  "Chrysler",
+  "Citroen",
+  "Dodge",
+  "Ferrari",
+  "Fiat",
+  "Ford",
+  "GMC",
+  "Honda",
+  "Hyundai",
+  "Infiniti",
+  "Jaguar",
+  "Jeep",
+  "Kia",
+  "Lamborghini",
+  "Land Rover",
+  "Lexus",
+  "Lincoln",
+  "Maserati",
+  "Mazda",
+  "McLaren",
+  "Mercedes-Benz",
+  "MINI",
+  "Mitsubishi",
+  "Nissan",
+  "Porsche",
+  "Ram",
+  "Rolls-Royce",
+  "Subaru",
+  "Tesla",
+  "Toyota",
+  "Volkswagen",
+  "Volvo",
+];
 
 const SearchManufacturer = ({ manufacturer, setManufacturer }) => {
   const [query, setQuery] = useState("");
@@ -16,41 +59,42 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }) => {
         );
 
   return (
-    <div className="search-manufacturer">
+    <div className='search-manufacturer'>
       <Combobox value={manufacturer} onChange={setManufacturer}>
-        <div className="relative w-full">
-          <Combobox.Button className="absolute top-[14px]">
+        <div className='relative w-full'>
+          <Combobox.Button className='absolute top-[14px]'>
+            {/* put the path to the image link in this directly vey */}
             <img
-              src="/car-logo.svg"
+              src='/car-logo.svg'
               width={20}
               height={20}
-              className="ml-4"
-              alt="car logo"
+              className='ml-4'
+              alt='car logo'
             />
           </Combobox.Button>
 
           <Combobox.Input
-            className="search-manufacturer__input"
+            className='search-manufacturer__input'
             displayValue={(item) => item}
             onChange={(event) => setQuery(event.target.value)}
-            placeholder="Volkswagen..."
+            placeholder='Volkswagen...'
           />
 
           <Transition
             as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
             afterLeave={() => setQuery("")}
           >
             <Combobox.Options
-              className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+              className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
               static
             >
               {filteredManufacturers.length === 0 && query !== "" ? (
                 <Combobox.Option
                   value={query}
-                  className="search-manufacturer__option"
+                  className='search-manufacturer__option'
                 >
                   Create "{query}"
                 </Combobox.Option>
@@ -67,18 +111,11 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }) => {
                   >
                     {({ selected, active }) => (
                       <>
-                        <span
-                          className={`block truncate ${
-                            selected ? "font-medium" : "font-normal"
-                          }`}
-                        >
+                        <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
                           {item}
                         </span>
                         {selected ? (
-                          <span
-                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                              active ? "text-white" : "text-primary-purple"
-                            }`}
+                          <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active? "text-white": "text-primary-purple"}`}
                           ></span>
                         ) : null}
                       </>
